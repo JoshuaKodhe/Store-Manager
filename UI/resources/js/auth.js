825d307e925fe61af32877110ac6cab5f30e68fc
Joshua Kodhe <jkodhe32@gmail.com>
2018-11-10 16:39:28 +0300

    [Feature #161859741] Add user login into our application

---
+++ UI/resources/js/auth.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+
+document.getElementById("loginUser").addEventListener("submit", loginUser);
+
+function loginUser(e){
+	e.preventDefault();
+
+	let email = document.getElementById("email").value;
+	let password = document.getElementById('password').value;
+
+	fetch('http://127.0.0.1:5000/api/v2/auth/login',{
+		method:'POST',
+		headers:{
+		'Access-Control-Allow-Origin':'*',
+		'Access-Control-Request-Method': '*',
+		'Content-Type': 'application/json'
+		},
+		body:JSON.stringify({email, password})
+	})
+	.then((res) => res.json())
+	.then((data) => {
+		if(data.message == `Logged in as ${email}`){
+			localStorage.setItem('token', data.access_token);
+			window.location.href='products.html';
+		}else{
+			throw new Error(data.message);
+		}
+	})
+	.catch((err) => {
+		document.getElementById('response').style.color = 'red';
+		document.getElementById('response').innerHTML = err.message;
+	});
+
+}
